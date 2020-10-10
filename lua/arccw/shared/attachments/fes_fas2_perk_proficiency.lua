5d74dcb6db180066aaa66da986c7315415050ca1
Fesiug <publicfesiug@outlook.com>
2020-10-11 02:39:01 +0800

    SOH/proficency reloads (M4)

---
+++ lua/arccw/shared/attachments/fes_fas2_perk_proficiency.lua
@@ -0,0 +1,9 @@
+att.PrintName = "Proficiency"
+att.Icon = Material("entities/acwatt_perk_fastreload.png")
+att.Description = "Reload good."
+att.Desc_Pros = {
+	"Very cool and fast and swift reloads"
+}
+att.Desc_Cons = {
+}
+att.Slot = "fas2_perk"
