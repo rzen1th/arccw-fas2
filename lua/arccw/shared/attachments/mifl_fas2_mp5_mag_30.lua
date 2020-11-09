81403f2ed706e43400f5f06e49b319aba15efdc7
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2020-11-09 18:52:42 +0700

    sound 4

--- lua/arccw/shared/attachments/mifl_fas2_mp5_mag_30.lua
+++ lua/arccw/shared/attachments/mifl_fas2_mp5_mag_30.lua
@@ -17,12 +17,11 @@ att.Mult_Recoil = 1.25
 att.Mult_RecoilSide = 1.33
 att.Mult_ReloadTime = 1.05
 att.Mult_Range = 0.9
-
-att.Mult_ShootPitch = 0.8
+att.Mult_RPM = 0.9
 att.AddSuffix = "/10"
 
 att.Hook_GetShootSound = function(wep, fsound)
-    if fsound == "weapons/arccw_mifl/fas2/mp5/mp5_fire1.wav" then return "weapons/arccw_mifl/fas2/mp5/mp5_40cal_fire1.wav" end
-    if fsound == "weapons/arccw_mifl/fas2/mp5/mp5k_suppressed_fire1.wav" then return "weapons/arccw_mifl/fas2/mp5/mp5_40cal_suppressed_fire1.wav" end
+    if fsound == "weapons/arccw_mifl/fas2/mp5/mp5_fire1.wav" then return "weapons/arccw_mifl/fas2_custom/mp5/30.wav" end
+    if fsound == "weapons/arccw_mifl/fas2/mp5/mp5k_suppressed_fire1.wav" then return "weapons/arccw_mifl/fas2_custom/mp5/30sd.wav" end
 	
 end
