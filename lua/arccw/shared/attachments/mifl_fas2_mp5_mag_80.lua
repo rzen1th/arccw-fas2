9a65fce5626938b860eb876590848d35cea5833e
Fesiug <40924091+Fesiug@users.noreply.github.com>
2020-11-09 06:19:39 -0500

    80rnd and 25 split

--- lua/arccw/shared/attachments/mifl_fas2_mp5_mag_80.lua
+++ lua/arccw/shared/attachments/mifl_fas2_mp5_mag_80.lua
@@ -1,6 +1,6 @@
-att.PrintName = "80-Round .223"
+att.PrintName = "80-Round 9mm"
 att.Icon = Material("entities/arccw_mifl_fas2_mp5_mag_80.png", "mips smooth")
-att.Description = "High capacity Vollmer magazine firing experimental .223R rounds that fragment."
+att.Description = "High capacity Vollmer magazine."
 att.SortOrder = 80
 att.Desc_Pros = {
     "pro.magcap"
@@ -15,9 +15,3 @@ att.Override_ClipSize = 80
 att.Mult_SightedSpeedMult = 0.8
 att.Mult_SightTime = 1.2
 att.Mult_ReloadTime = 1.3
-att.AddSuffix = "/X223"
-
-att.Mult_AccuracyMOA = 10
-att.Mult_HipDispersion = 1.5
-att.Override_Num = 4
-att.Mult_ShootPitch = 0.5
---
+++ lua/arccw/shared/attachments/mifl_fas2_mp5_mag_waffle.lua
@@ -0,0 +1,23 @@
+att.PrintName = "25-Round X.223"
+att.Icon = Material("entities/arccw_mifl_fas2_mp5_mag_80.png", "mips smooth")
+att.Description = "High capacity Vollmer magazine firing experimental .223R rounds that fragment."
+att.SortOrder = 25
+att.Desc_Pros = {
+    "pro.magcap"
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "mifl_fas2_mp5_mag"
+
+att.Override_ClipSize = 80
+
+att.Mult_SightedSpeedMult = 0.8
+att.Mult_SightTime = 1.2
+att.Mult_ReloadTime = 1.3
+att.AddSuffix = "/X223"
+
+att.Mult_AccuracyMOA = 10
+att.Mult_HipDispersion = 1.5
+att.Override_Num = 4
+att.Mult_ShootPitch = 0.5
