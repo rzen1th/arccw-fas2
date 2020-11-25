cc276e5aa2fccf0cbe7867dd7411d5a46f17b63c
TheOnly8Z <888888zombies@gmail.com>
2020-11-25 01:30:35 -0500

    AK 9x39 and some magazine stats

--- lua/arccw/shared/attachments/mifl_fas2_ak_mag_545.lua
+++ lua/arccw/shared/attachments/mifl_fas2_ak_mag_545.lua
@@ -1,6 +1,6 @@
 att.PrintName = "30-Round 5.45mm"
 att.Icon = Material("entities/arccw_mifl_fas2_sg55x_m_762.png", "mips smooth")
-att.Description = ""
+att.Description = "Convert the weapon into the modern AK-74, firing an intermediate cartridge."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -11,25 +11,12 @@ att.Slot = "mifl_fas2_ak_mag"
 
 att.ActivateElements = {"30_545"}
 
-att.Mult_Recoil = 1
-att.Mult_RecoilSide = 1
-att.Mult_VisualRecoilMult = 1
+att.Mult_Damage = 0.9
+att.Mult_DamageMin = 0.9
+att.Mult_Penetration = 0.8
+att.Mult_Range = 0.8
+att.Mult_Recoil = 0.8
+att.Mult_RPM = 1.1
 
-att.Mult_RPM = 1.2
-att.Mult_MoveSpeed = 1
-att.Mult_Damage =1
-att.Mult_DamageMin = 1
-att.Mult_Range = 1
-att.Mult_Penetration = 1
-att.Mult_MuzzleVelocity = 1
-att.Mult_ReloadTime = 1
-att.Override_ClipSize = 30
-
-att.Hook_GetShootSound = function(wep, fsound)
-	if fsound == "weapons/arccw_mifl/fas2/sg55x/sg552_fire1.wav" then 
-		return "weapons/arccw_mifl/fas2/m249/m249_fire1.wav"
-    end
-	if fsound == "weapons/arccw_mifl/fas2/sg55x/sg552_suppressed_fire1.wav" then
-		return "weapons/arccw_mifl/fas2/m249/m249_suppressed_fire1.wav"
-    end
-end
+att.Override_Trivia_Calibre = "5.45x39mm"
+att.Override_Ammo = "smg1"
--- lua/arccw/shared/attachments/mifl_fas2_ak_mag_939_10.lua
+++ lua/arccw/shared/attachments/mifl_fas2_ak_mag_939_10.lua
@@ -1,41 +1,25 @@
 att.PrintName = "10-Round 9x39mm"
 att.Icon = Material("entities/arccw_mifl_fas2_sg55x_m_762.png", "mips smooth")
-att.Description = ""
-att.Desc_Pros = {
-}
-att.Desc_Cons = {
-	"con.magcap"
-}
+att.Description = "Load 9x39mm cartridges, designed for subsonic velocities in suppressed weapons. Lighter magazine improves handling."
+att.Desc_Pros = {}
+att.Desc_Cons = {"con.magcap"}
 att.SortOrder = 10
 att.AutoStats = true
 att.Slot = "mifl_fas2_ak_mag"
 
+-- 10rnd stats
 att.ActivateElements = {"10_939"}
-
-att.Mult_Recoil = 1
-att.Mult_RecoilSide = 1
-att.Mult_VisualRecoilMult = 1
-
-att.Mult_RPM = 1.5
-att.Mult_MoveSpeed = 1
-att.Mult_Damage =1
-att.Mult_DamageMin = 1
-att.Mult_Range = 1
-att.Mult_Penetration = 1
-att.Mult_MuzzleVelocity = 1
-att.Mult_ReloadTime = 1
 att.Override_ClipSize = 10
+att.Mult_MoveSpeed = 1.1
+att.Mult_SightTime = 0.85
+att.Mult_ReloadTime = 0.9
 
-att.Hook_GetShootSound = function(wep, fsound)
-	if fsound == "weapons/arccw_mifl/fas2/sg55x/sg552_fire1.wav" then 
-		return "weapons/arccw_mifl/fas2/m249/m249_fire1.wav"
-    end
-	if fsound == "weapons/arccw_mifl/fas2/sg55x/sg552_suppressed_fire1.wav" then
-		return "weapons/arccw_mifl/fas2/m249/m249_suppressed_fire1.wav"
-    end
-end
+-- 7.62x39 -> 9x39mm stats
+att.Mult_Damage = 0.8
+att.Mult_DamageMin = 0.9
+att.Mult_Recoil = 0.7
+att.Mult_ShootVol = 0.85
+att.Mult_RPM = 1.2
 
 att.Override_Trivia_Calibre = "9x39mm"
 att.Override_Ammo = "smg1"
-
-att.AddSuffix = "/9"
