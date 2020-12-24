7512c7283e6aee2ba7512fe6211ce896b4b41b9f
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2020-12-25 06:45:45 +0700

    daddy uwu

--- lua/arccw/shared/attachments/mifl_fas2_m11_muz_sd.lua
+++ lua/arccw/shared/attachments/mifl_fas2_m11_muz_sd.lua
@@ -9,3 +9,10 @@ att.SortOrder = 1
 
 att.Slot = "mifl_fas2_m11_muz"
 att.AutoStats = true
+
+att.LHIK = true
+att.LHIK_Priority = -2
+
+att.Model = "models/weapons/arccw/mifl_atts/fas2/grip_famas_sd.mdl"
+
+att.ModelOffset = Vector(5, 0.2, 0)
--- lua/arccw/shared/attachments/mifl_fas2_m11_muz_thomp.lua
+++ lua/arccw/shared/attachments/mifl_fas2_m11_muz_thomp.lua
@@ -9,3 +9,10 @@ att.SortOrder = 2
 
 att.Slot = "mifl_fas2_m11_muz"
 att.AutoStats = true
+
+att.LHIK = true
+att.LHIK_Priority = -2
+
+att.Model = "models/weapons/arccw/mifl_atts/fas2/grip_famas_felin.mdl"
+
+att.ModelOffset = Vector(0.5, -0.3, -1.8)
