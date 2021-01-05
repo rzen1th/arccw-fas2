140ded8bd7085856837eaa149aab4921b9d0915e
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2021-01-05 13:23:28 +0700

    credits update

--- README.md
+++ README.md
@@ -1,42 +1,47 @@
 "Top text, bottom text"
 
-19 Weapons, 206 Attachments. It's a lot of stuff 
+19 Weapons, 218 Attachments. It's a lot of stuff 
 
 The most customisable weapon pack on the workshop with ludicrous amount of fictional customisations for your heart's content
 
 -Overview-
 
+Development starts shortly after the disappointment (to me) that is Gunsmith Offensive
+
 Featuring nearly every weapon from Firearms Source 2 (and some custom one) packed with kitbashed attachments
 
 If GSO is Bootleg Modern Warfare. [Insert name] is Bootleg Cold War
 
-Barrels, handguard, stocks, mag conversions, everything you expect, handcrafted for each and every firearms
+Barrels, handguard, stocks, mag conversions, everything you expect, handcrafted for each and every firearms.
 
-Unlike the stubborn cunt who made GSO. This features full on ~~slightly wonky~~ AKIMBO system for all pistols* and SMGs
+Unlike the stubborn cunt who made GSO. This features full on ~~slightly wonky~~ AKIMBO system for all pistols* and SMGs.
 
-*The main focus of this mod is not realism so don't complain about that*
+*The main focus of this mod is not realism so don't complain about that*.
 
 -Credits-
+
+Fluffy Mammal - Model Hacking, Animation Tweaking.
 
 TheOnly8Z - Major helping, Coding, General Things.
 
 Fesiug - QCI, Akimbo System, Coding, Spawnicons.
 
-FAS2 Team - Assets
+FAS2 Team - Assets.
 
 -Extra-
 
-Killing Floor 2 - Assets
+Killing Floor 2 - Assets.
 
-Infinity Ward - Assets 
+Infinity Ward - Assets .
 
-DICE - Assets
+DICE - Assets.
 
 [...]
-more on them in the credits file of the mod
+
+more on them in the credits file of the mod.
 
 -Extra-
 
 
 
-feedbacks and bugs report are appreciated, request and suggestions are not
+feedbacks and bugs report are appreciated, request and suggestions are not.
--- lua/arccw/documentation/fas2_credits.lua
+++ lua/arccw/documentation/fas2_credits.lua
@@ -1,45 +1,14 @@
---- who even read this ---
-
-
-
-
-GSO is garbage lmao
-its mediocre at best and you cant convince me otherwise
-adding nothing new compare to cs+ aside from -owo slight model change- with basically the same stat modifier for every-bloody-thing 
-even fucking less customizability in general
-
-
-Thats why I decide to utilize arccw to the fullest and push it to a level "not seen" before
-With the aim of
-1. The sheer amount of unique attachments (both fictional and realistic)
-2. Freedom of choice (its a fucking game lmao not everything have to be bounded by reality, I really hope people would just fuck off and just accept that this is a fucking game and not some sort of simulator)
-3. Bored lol 
-4. 4th point
-5. how 2 blender 2.9
-
 --- to the 5 people that decompiled this mod for whatever reason and read this
 --- please dont fucking make cheaty attachment or anything like that
 --- thank you very much
 
 
-[short credits]
-/*
-Fesiug - QCI, some refining
-TheOnly8Z - Major refining
-oranchepopsicle - Bottom Text 
-Fluffy Mammal - take a wild guess
-FA:S 2.0 - Assets
 
-- Extra -
-Killing Floor 2 - Assets
-Infinity Ward - Assets 
-DICE - Assets
-- Extra -
+
+
 
 
 --- look ight these nibbas dont really give specific credits ---
-
-
 M16A1 - ???
 https://gamebanana.com/skins/19381
 
@@ -58,46 +27,50 @@ https://gamebanana.com/skins/126616
 VOLKED KIT - ???
 http://gamer-lab.com/eng/mdl_source/X-90
 
-XM8 - End of Days
+XM8 - End of Days -- scrapped --
 https://gamebanana.com/skins/41834
 
 ---------------------------------------------------------------
-In Depth Credits
-[blank slot] 
--[insert thing]
+In Depth Credits		(using steam names)
+Fluffy Mammal 
+-model hacks
+-anims edits
+-MASS26
+-casing models
 
 Fesiug
 -akimbo (barely works but ok)
--rewriting nomen, reloading code
--sexy damped burst
+-rewriting nomen
+-damped burst
 -spawnicon
 
 TheOnly8Z
 -clean up crew
 -rebalance 
 -naming system
+-lotsa coding
+-m79 grenades 
 -stuff
-
-oranchepopsicle
--funi descriptions and names [weapon/attachment]
 
 Tripwire (Killing Floor 2)
 -HEAT Barrel (M4)
 -EOD Barrel (MP5) 
 -10mm Mag (MP5) 
--Thompson M1911 thing
+-Thompson Kit
 -UMP45 (MP5) 
--Rail extender thing 
+-RIS Mount (M11)
 -M3 Stock
 -G18c/G17 Slide
--Gluke Slide
+-Glock Stock
+-M3 parts
 
-Infinity Ward / Beenox / Treyarch
+Infinity Ward / Beenox / Treyarch 
 -9mm Adaptor (MW19)
 -Pistol Rail(MW2)
 -Raptor Kit (MW2CR)
 -AN94 (BO3)
 -Single Drum Mag (MW19)
+-Jungle Wraps (MWR)
 -Suppressed sound and firing sound for calibre conversion (Mostly MW19)
 
 Saber Interactive (AVA) -- quite ebin assets ngl
@@ -108,10 +81,14 @@ Crytek (Warface)
 -AS Val 
 -FO12
 
-DICE (BF4)
--M26
--M320
--AK5C
+DICE 
+-M26 (BF4)
+-M320 (BF4)
+-GP30 (BF4)
+-SAIGA 12 (BF3)
+
+They Hunger 
+-TOZ34
 
 FA:S2 
 -your guess is as good as mine
@@ -127,19 +104,3 @@ Google Images
 -Sirgibsalot (kf2,smod,half life 2 face fix and alot other stuff, this guy is a fucking chad)
 -HK G41/empty (ava, warface rip, pretty fucking sure these two are the same)
 -ClearSkyC (warface rip)
-*/
-
-
-
-
-
-
-
-
-Hope you fucking enjoy
-Im done with this shit game
-Im fucking done with this shit head of a community
-Yall fucking cunts and I hate all of you
-With that I say goodbye
-Fare well cunts
-Hope we never meet again
