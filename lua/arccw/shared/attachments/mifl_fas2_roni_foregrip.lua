101bbbe7892821032dd0c8bafe7a8f70a2a2555e
TheOnly8Z <8888888zombies@gmail.com>
2021-01-17 16:57:44 +0800

    Roni grip stats

--- lua/arccw/shared/attachments/mifl_fas2_roni_foregrip.lua
+++ lua/arccw/shared/attachments/mifl_fas2_roni_foregrip.lua
@@ -1,9 +1,6 @@
-att.PrintName = "Folding Foregrip"
+att.PrintName = "Roni Foregrip"
 att.Icon = nil ---Material("entities/acwatt_milf_grip_side.png")
-att.Description = "Ripped straight from a RONI kit. decent recoil control."
-
-att.SortOrder = 1
-
+att.Description = "Folding foregrip from a RONI carbine kit. Provides limited recoil and hipfire control."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -15,11 +12,9 @@ att.LHIK = true
 
 att.Model = "models/weapons/arccw/mifl_atts/fas2_roni_grip.mdl"
 
-att.Mult_Recoil = 0.95
-
-att.Mult_SightTime = 1.05
-att.Mult_HipDispersion = 0.7
-att.Mult_SpeedMult = 1.1
-att.Mult_SightedSpeedMult = 1.1
+att.Mult_Recoil = 0.9
+att.Mult_HipDispersion = 0.9
+att.Mult_SightTime = 1.1
+att.Mult_SightedSpeedMult = 0.85
 
 att.ModelOffset = Vector(-5.5, 0, -1.2)
--- lua/arccw/shared/attachments/mifl_fas2_roni_foregrip2.lua
+++ lua/arccw/shared/attachments/mifl_fas2_roni_foregrip2.lua
@@ -1,9 +1,6 @@
-att.PrintName = "Half-Folded Foregrip"
+att.PrintName = "Roni Angled Foregrip"
 att.Icon = nil ---Material("entities/acwatt_milf_grip_side.png")
-att.Description = "Half closed folding grip from a RONI kit. effectively and AFG."
-
-att.SortOrder = 1
-
+att.Description = "Half closed folding grip from a RONI carbine kit. Improves handling in a fashion similar to an AFG."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -11,15 +8,14 @@ att.Desc_Cons = {
 att.AutoStats = true
 att.Slot = "foregrip"
 
+att.InvAtt = "mifl_fas2_roni_foregrip"
 att.LHIK = true
 
 att.Model = "models/weapons/arccw/mifl_atts/fas2_roni_marksman_grip.mdl"
 
-att.Mult_Recoil = 1.05
-
-att.Mult_SightTime = 1.2
+att.Mult_SightTime = 0.9
+att.Mult_Recoil = 0.9
+att.Mult_SightedSpeedMult = 0.85
 att.Mult_HipDispersion = 1.1
-att.Mult_SpeedMult = 1.2
-att.Mult_SightedSpeedMult = 1.25
 
 att.ModelOffset = Vector(-5.5, 0, -1.2)
