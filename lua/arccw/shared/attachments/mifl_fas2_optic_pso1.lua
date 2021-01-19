a3dc630da974f4bbd59a707caef402daa0248395
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2021-01-19 20:56:02 +0700

    zooom

--- lua/arccw/shared/attachments/mifl_fas2_optic_pso1.lua
+++ lua/arccw/shared/attachments/mifl_fas2_optic_pso1.lua
@@ -18,7 +18,7 @@ att.HolosightPiece = "models/weapons/arccw/mifl_atts/fas2_optic_pso1_hsp.mdl"
 
 att.AdditionalSights = {
     {
-        Pos = Vector(0, 9, -1.6),
+        Pos = Vector(0, 8.5, -1.6),
         Ang = Angle(0, 0, 0),
         Magnification = 1.2,
         ScrollFunc = ArcCW.SCROLL_ZOOM,
@@ -31,9 +31,9 @@ att.HolosightMagnificationMin = 4
 att.HolosightMagnificationMax = 4
 att.HolosightReticle = Material("mifl_fas2_reticle/pso1.png", "mips smooth")
 att.HolosightNoFlare = true
-att.HolosightSize = 6
+att.HolosightSize = 7
 att.HolosightBone = "holosight"
-att.Colorable = false
+att.Colorable = true
 att.HolosightBlackbox = false --- for now
 
 att.Mult_SightTime = 1.08
