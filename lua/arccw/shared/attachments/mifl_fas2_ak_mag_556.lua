f3b1d16e9fdee808f95e8ef086bcfac600a7e52d
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2021-01-21 21:03:08 +0700

    changed my mind

--- lua/arccw/shared/attachments/mifl_fas2_ak_mag_556.lua
+++ lua/arccw/shared/attachments/mifl_fas2_ak_mag_556.lua
@@ -16,7 +16,8 @@ att.Mult_DamageMin = 0.85
 att.Mult_Penetration = 0.8
 att.Mult_Range = 0.8
 att.Mult_Recoil = 0.7
-att.Mult_RPM = 1.25
+att.Mult_RecoilSide = 0.5
+att.Mult_RPM = 1.35
 
 att.Override_Ammo = "smg1"
 
