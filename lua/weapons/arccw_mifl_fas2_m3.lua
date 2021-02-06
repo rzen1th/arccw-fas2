8d2c5dd4da0bae916d8477e32b7ecf6e82d2fbdd
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2021-02-06 15:02:59 +0700

    sound path

--- lua/weapons/arccw_mifl_fas2_m3.lua
+++ lua/weapons/arccw_mifl_fas2_m3.lua
@@ -71,9 +71,9 @@ SWEP.Primary.Ammo = "buckshot" -- what ammo type the gun uses
 SWEP.ShootVol = 120 -- volume of shoot sound
 SWEP.ShootPitch = 100 -- pitch of shoot sound
 
-SWEP.ShootSound = "weapons/arccw_mifl/fas2/m3s90p/m3s90_fire1.wav"
-SWEP.ShootSoundSilenced = "arccw_go/m590_suppressed_fp.wav"
-SWEP.DistantShootSound = "arccw_go/nova/nova-1-distant.wav"
+SWEP.ShootSound = "weapons/arccw_mifl/fas2/m3s90/m3s90_fire1.wav"
+SWEP.ShootSoundSilenced = "weapons/arccw_mifl/fas2/rem870/sd_fire.wav"
+SWEP.DistantShootSound = "weapons/arccw_mifl/fas2/m3s90/m3s90_distance_fire1.wav"
 
 SWEP.MeleeSwingSound = "arccw_go/m249/m249_draw.wav"
 SWEP.MeleeMissSound = "weapons/iceaxe/iceaxe_swing1.wav"
