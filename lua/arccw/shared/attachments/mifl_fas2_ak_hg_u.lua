935d41e786014acad20222d26a2812a9b87a6681
Fesiug <40924091+Fesiug@users.noreply.github.com>
2021-02-16 22:52:34 -0500

    Update mifl_fas2_ak_hg_u.lua

--- lua/arccw/shared/attachments/mifl_fas2_ak_hg_u.lua
+++ lua/arccw/shared/attachments/mifl_fas2_ak_hg_u.lua
@@ -28,6 +28,6 @@ att.LHIK_Priority = -2
 
 att.Model = "models/weapons/arccw/mifl_atts/fas2/grip_famas_k.mdl"
 
-att.ModelOffset = Vector(2, 0, 0)
+att.ModelOffset = Vector(2, 0, -0.25)
 
 att.GivesFlags = {"ubgl_no"}
