ddd4d5aab3935596bf6ea40908277d6e941471a2
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2021-02-19 15:21:28 +0700

    poo

--- lua/weapons/arccw_mifl_fas2_rpk.lua
+++ lua/weapons/arccw_mifl_fas2_rpk.lua
@@ -691,18 +691,18 @@ SWEP.Animations = {
         Source = "reload_nomen_helix",
         TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
         LHIK = true,
-        LHIKEaseIn = 0.5,
-        LHIKIn = 0.8,
-        LHIKOut = 0.7,
-        LHIKEaseOut = 0.4
+        LHIKEaseIn = 0.4,
+        LHIKIn = 0.6,
+        LHIKOut = 0.5,
+        LHIKEaseOut = 0.3
     },
     ["reload_nomen_empty_helix"] = {
         Source = "reload_nomen_empty_helix",
         TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
         LHIK = true,
-        LHIKEaseIn = 0.5,
-        LHIKIn = 1.2,
-        LHIKOut = 2,
-        LHIKEaseOut = 0.5
+        LHIKEaseIn = 0.4,
+        LHIKIn = 0.8,
+        LHIKOut = 1.8,
+        LHIKEaseOut = 0.4
     },
 }
