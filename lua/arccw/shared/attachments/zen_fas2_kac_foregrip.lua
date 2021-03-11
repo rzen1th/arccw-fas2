8201a53eb62bcf5e3b157322b735ef5599a546d3
rzen1th <pierce4077m@gmail.com>
2021-03-10 22:46:26 -0600

    unfuck
    i hate life

--- lua/arccw/shared/attachments/zen_fas2_kac_foregrip.lua
+++ lua/arccw/shared/attachments/zen_fas2_kac_foregrip.lua
@@ -1,6 +1,6 @@
-att.PrintName = "KAC Vertical Pistol Grip"
-att.Icon = Material("entities/arccw_mifl_roni_grip.png", "mips smooth")
-att.Description = "Zenith does something."
+att.PrintName = "KAC Forward Grip"
+att.Icon = Material("entities/zen_fas2_foregrip_kac.png", "mips smooth")
+att.Description = "Polymer vertical foregrip. Stable aiming platform helps keep the weapon steady and decreases recoil."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -12,11 +12,10 @@ att.LHIK = true
 
 att.Model = "models/weapons/arccw/mifl_atts/fas2_vfg.mdl"
 
-att.Mult_Recoil = 0.9
-att.Mult_HipDispersion = 0.9
-att.Mult_SightTime = 1.1
-att.Mult_SightedSpeedMult = 0.85
+att.Mult_Recoil = 0.75
+att.Mult_SightTime = 1.05
+att.Mult_SightedSpeedMult = 0.7
 
-att.ModelOffset = Vector(-27.762, -5.592, -4.124)
+att.ModelOffset = Vector(-27.727, -4.115, 5.845)
 
 att.Override_HoldtypeActive = "smg"
